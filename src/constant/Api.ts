f52445786bb821d99d5db3f92ea2b28a483d9cd6
MoisesINMDeveloper <moisesisraelnavarro@gmail.com>
2025-02-03 09:49:58 -0400

    Deploy

--- src/constant/Api.ts
+++ src/constant/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://ujap-cafetin-server-production.up.railway.app";
 
 //--Servicios de Usuarios--//
 export const loginAdmin = async (username: string, password: string) => {
